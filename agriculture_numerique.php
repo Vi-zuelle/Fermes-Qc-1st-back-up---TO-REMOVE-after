b20fbad4c072afdfd602e29fde24c4be3c5bac31
Vi <vipannetier@gmail.com>
2021-03-12 15:16:45 -0500

    creation page agriculture_numerique + image claude3.jpg

---
+++ agriculture_numerique.php
@@ -0,0 +1,256 @@
+
+<!DOCTYPE html>
+<!--[if lte IE 8]>
+<html class="ie8 no-js" lang="en">     <![endif]-->
+<!--[if IE 9]>
+<html class="ie9 no-js" lang="en-US">  <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="not-ie no-js" lang="en">  <!--<![endif]-->
+<head>
+    <!-- Google Web Fonts
+    ================================================== -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,300italic,400,400italic,500,700%7cCourgette%7cRaleway:400,700,500%7cCourgette%7cLato:700'
+          rel='stylesheet' type='text/css'>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title>Agriculture numérique - FERMES QUÉBEC - Photographie vidéo cartographie aériennes</title>
+    <meta name="description" content="Fermes du Québec : Photos et vidéos aériennes de grande qualité (obliques, orghogonales, HD, 4K) de votre ferme ou propriété agricole par des photographes aériens maîtres du ciel" />
+    <META NAME="keywords" CONTENT="fermes québec,photographe aérien,photo aérienne,ferme,Québec,photos aériennes,vidéos,propriété agricole,fermette,Claude Duchaine,pilote,patrimoine,histoire des fermes au Québec,agriculture,fermes du Québec,Ferme Québec,fermes.quebec,Canada,archives,archive" />
+    <meta name="author" content="Claude Duchaîne, Nicole Deschamps Inter-actif Communications, Johanne Berry,Air Imex,Airimex" />
+
+    <!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/grid.css"/>
+    <link rel="stylesheet" href="css/layout.css"/>
+    <link rel="stylesheet" href="css/fontello.css"/>
+    <link rel="stylesheet" href="css/animation.css"/>
+
+    <!-- HTML5 Shiv
+    ================================================== -->
+    <script src="js/jquery.modernizr.js"></script>
+
+
+</head>
+<body class="animated loaded" oncontextmenu="return false" ondragstart="return false" onselectstart="return false">
+<?php include_once("analyticstracking.php") ?>
+<div id="wrapper">
+
+    <div id="mobile-menu" class="mobile-menu">
+
+        <a href="#" id="menu-hide"></a>
+
+    </div>
+
+
+    <!-- - - - - - - - - - - - - Header - - - - - - - - - - - - - - -->
+
+
+    <header id="header">
+
+      <div class="top-header-line">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-xs-12">
+                        <div class="contact-icons clearfix">
+                            <ul class="contact-details">
+                                <li><i class=" icon-mail-5"></i>Courriel: <a href="mailto:info@fermes.quebec">info@fermes.quebec</a></li>
+                                <li><i class="icon-phone-2"></i>514-953-2434</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+                <!--/ .row-->
+
+            </div>
+            <!--/ .container-->
+
+        </div>
+
+        <!--/ .top-header-line-->
+
+        <div class="middle-header-line">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-12 col-sm-10">
+
+                        <div class="header-in">
+
+                            <h1 id="logo">
+                                <a href="index.php"><img src="images/logo.png"></a>
+                            </h1>
+
+                            <a id="responsive-nav-button" class="responsive-nav-button" href="#"></a>
+
+                            <div class="nav-wrapper">
+                            <nav id="navigation" class="navigation">
+                                <ul>
+                                    <li><a href="index.php">ACCUEIL</a></li>
+                                 <li><a href="galerie.php">GALERIE PHOTOS ET VIDÉOS</a></li>
+                                    <!--<li><a href="fermes.php">Fermes Québec</a></li>-->
+                                    <li><a href="agriculture_numerique.php">AGRICULTURE NUMÉRIQUE</a></li>
+                                    <li class="current-menu-item"><a href="services.php">FORFAITS</a></li>
+                                      <li><a href="equipe.php">QUI SOMMES-NOUS?</a></li>
+                                    <!--  <li><a href="carte_des_fermes.php">CARTE</a></li>-->
+                                    <li><a href="contacts.php">CONTACT</a></li>
+                                </ul>
+
+                            </nav>
+                                <!--/ .navigation-->
+
+                            </div>
+                            <!--/ .nav-wrapper-->
+
+                        </div>
+                        <!--/ .header-in-->
+
+                    </div>
+
+                </div>
+                <!--/ .row-->
+
+            </div>
+            <!--/ .container-->
+
+        </div>
+        <!--/ .middle-header-line-->
+
+    </header>
+    <!--/ #header-->
+
+    <!-- - - - - - - - - - - - - end Header - - - - - - - - - - - - - - -->
+
+
+    <!-- - - - - - - - - - - - - Content - - - - - - - - - - - - - - - -->
+
+    <div id="content">
+
+        <!--/ .section-->
+
+        <section class="section padding-top-off">
+
+            <div class="container">
+
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="section-title opacity">
+                    <h2>AGRICULTURE NUMÉRIQUE</h2>
+                </div>
+              </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-6 col-xs-12">
+                <img src="images/agriculture_numerique/claude3.jpg" alt="Claude Duchaine Président Fermes Québec">
+              </div>
+              <div class="col-md-6 col-xs-12">
+                <p>Depuis nos débuts en 1992, les producteurs agricoles ont dû compétitionner mondialement avec des pays aux climats plus chauds, s’adapter aux extrêmes de la météo, au manque de personnel et à la diminution des marges de profit tout en se conformant aux exigences environnementales des plus sévères au monde.</p>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            </div>
+            <!--/ .container-->
+
+        </section>
+        <!--/ .section-->
+
+    </div>
+    <!--/ #content-->
+
+
+    <!-- - - - - - - - - - - - end Content - - - - - - - - - - - - - -->
+
+
+    <!-- - - - - - - - - - - - - - Footer - - - - - - - - - - - - - - -->
+
+
+    <footer id="footer">
+
+        <div class="entry-footer parallax parallax-bg-2">
+
+            <div class="full-bg-image"></div>
+            <div class="overlay-paralax"></div>
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-xs-12">
+
+                        <div class="offers">
+                            <h1>VENEZ VOIR D'AUTRES EXEMPLES DE NOTRE TRAVAIL <br />CHEZ AIR IMEX LTÉE !</h1>
+                        </div>
+
+                    </div>
+
+                </div>
+                <!--/ .row-->
+
+            </div>
+            <!--/ .container-->
+
+        </div>
+
+        <!--/ .entry-footer-->
+
+		<?php include('pub_jaune.php');?>
+        <!--/ .clients-container-->
+
+		<?php include('footer.php');?>
+
+    </footer>
+    <!--/ #footer-->
+
+
+    <!-- - - - - - - - - - - - - end Footer - - - - - - - - - - - - - -->
+
+
+    <!-- - - - - - - - - - - - Bottom Footer - - - - - - - - - - - - -->
+
+
+
+
+    <!-- - - - - - - - - - - end Bottom Footer - - - - - - - - - - - -->
+
+
+</div>
+<!--/ #wrapper-->
+
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+<script src="js/plugins.js"></script>
+<script src="js/twitterFetcher_min.js"></script>
+<script src="js/jquery.smoothscroll.js"></script>
+<script src="js/jquery.resizegrid.js"></script>
+<script src="js/imagesloaded.js"></script>
+<script src="js/config.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
