df57f7cc9caccb2910738aed62f0afa2fbaa8a09
Vi <vipannetier@gmail.com>
2021-03-12 16:52:18 -0500

    35% page agriculture_numérique

--- agriculture_numerique.php
+++ agriculture_numerique.php
@@ -163,13 +163,76 @@
 
             <div class="row">
               <div class="col-md-6 col-xs-12">
-                <img src="images/agriculture_numerique/claude3.jpg" alt="Claude Duchaine Président Fermes Québec">
+                <img class="img-padding-card" src="images/agriculture_numerique/claude3.jpg" alt="Claude Duchaine Président Fermes Québec">
               </div>
               <div class="col-md-6 col-xs-12">
-                <p>Depuis nos débuts en 1992, les producteurs agricoles ont dû compétitionner mondialement avec des pays aux climats plus chauds, s’adapter aux extrêmes de la météo, au manque de personnel et à la diminution des marges de profit tout en se conformant aux exigences environnementales des plus sévères au monde.</p>
+                <p class="p-card-24">Depuis nos débuts en 1992, les producteurs agricoles ont dû compétitionner mondialement avec des pays aux climats plus chauds, s’adapter aux extrêmes de la météo, au manque de personnel et à la diminution des marges de profit tout en se conformant aux exigences environnementales des plus sévères au monde.</p>
               </div>
             </div>
             <!--/ .row-->
+
+            <div class="row">
+              <div class="col-12">
+                <p class="p-general-24">Pour travailler dans ces conditions, les producteurs doivent prendre des décisions complexes très importantes afin d’assurer la pérennité de leurs entreprises.</p>
+                <p class="p-general-24">Fort heureusement, la plupart des producteurs ont relevé ces défis en misant sur l’aide des agronomes – conseils et sur leur propre formation. Ayant suivi attentivement les trois journées de formation en Technologies d’Agriculture de Précision du CRAAQ, je les recommande fortement à tous les intervenants. Quoi que assez intenses, vous pourrez continuer votre formation à votre rythme à l’aide d’excellentes notes de cours.</p>
+                <p class="p-general-24">En 2021, l’agriculture numérique est maintenant devenue un choix stratégique et pragmatique, qui tient compte des caractéristiques propres de chaque parcelle, améliore la productivité et répond aux attentes environnementales et sociétales.</p>
+                <p class="p-general-24">Les cartes d’agriculture numérique et leur interprétation sont devenues essentielles aux services – conseils et aux producteurs agricoles. Pour être efficaces, elles doivent être précises et livrées rapidement aux intervenants après leur captation.</p>
+                <p class="p-general-24">Après des années de recherche et de développement, FERMES QUÉBEC a adapté la technologie de pointe des drones à un avion piloté pour des performances optimales.</p>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-6 col-xs-12">
+                <img class="img-padding-card" src="images/agriculture_numerique/CONTROLEUR.jpg" alt="agriculture numerique controleur">
+                <p style="padding:0 8%;"><i>Le contrôleur de vol déclenche la caméra à tous les 60 mètres mesurés au sol.</i></p>
+              </div>
+              <div class="col-md-6 col-xs-12">
+                <p class="p-card-24">Cette combinaison gagnante permet de capter simultanément en visible, infra-rouge et multi spectral, couvrant 20 parcelles à l’intérieur d’un rayon de 30 Km dans une mission de vol d’une heure et demi, ou de couvrir 1000 acres linéaires par heure.</p>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-12">
+                <p class="p-general-24">En 2021, FERMES QUÉBEC offre aux agronomes – conseils et aux producteurs  agricoles un dossier complet d’agriculture numérique à <strong>100,00 $</strong> la parcelle comprenant 7 produits:</p>
+                <ol class="p-general-24">
+                  <li>Carte Orthomosaïque & Rapport de Qualité</li>
+                  <li>Carte santé végétale NDVI & Rapport de Qualité</li>
+                  <li>Carte d’Élévation DEM & Rapport de Qualité</li>
+                  <li>Modèle 3D</li>
+                  <li>Ortho Photos 50 Mega Pixels</li>
+                  <li>Ortho Vidéo 4 K</li>
+                  <li>Thermographies 740 Ko</li>
+                </ol>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-12">
+                <p class="p-general-24">Conditions: Les parcelles doivent être en groupes de 20 dans les secteurs de <span style="color:#548dd4;">Laurentides sud</span>, <span style="color:#ffd200;">Montérégie Est</span> ou <span style="color:#ff0000;">Montérégie Ouest<span>.</p>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-6 col-xs-12">
+                <!-- <p class="p-card-24">Actuellement, les missions de vol prévues comptent :</p> -->
+                <p class="p-card-24">
+                  Actuellement, les missions de vol prévues comptent :
+                  <ul class="p-general-24">
+                    <li>000  <span style="color:#548dd4;">Parcelles en Laurentides Sud</span></li>
+                    <li>000  <span style="color:#ffd200;">Parcelles en Montérégie Est</span></li>
+                    <li>000  <span style="color:#ff0000;">Parcelles en Montérégie Ouest</span></li>
+                  </ul>
+                </p>
+              </div>
+              <div class="col-md-6 col-xs-12">
+
+              </div>
+            </div>
+
 
             </div>
             <!--/ .container-->
