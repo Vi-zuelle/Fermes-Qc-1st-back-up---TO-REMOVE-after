59d3adc4f2f04a8d2a0c46cd44559644c983cff8
Vi <vipannetier@gmail.com>
2021-03-13 18:52:29 -0500

    70% page agriculture numerique

--- agriculture_numerique.php
+++ agriculture_numerique.php
@@ -151,7 +151,7 @@
 
         <section class="section padding-top-off">
 
-            <div class="container">
+          <div class="container">
 
             <div class="row">
               <div class="col-xs-12">
@@ -163,40 +163,40 @@
 
             <div class="row">
               <div class="col-md-6 col-xs-12">
-                <img class="img-padding-card" src="images/agriculture_numerique/claude3.jpg" alt="Claude Duchaine Président Fermes Québec">
+                <img class="p-all-4" src="images/agriculture_numerique/claude3.jpg" alt="Claude Duchaine Président Fermes Québec">
               </div>
               <div class="col-md-6 col-xs-12">
-                <p class="p-card-24">Depuis nos débuts en 1992, les producteurs agricoles ont dû compétitionner mondialement avec des pays aux climats plus chauds, s’adapter aux extrêmes de la météo, au manque de personnel et à la diminution des marges de profit tout en se conformant aux exigences environnementales des plus sévères au monde.</p>
+                <p class="paragraph m-0 p-all-8">Depuis nos débuts en 1992, les producteurs agricoles ont dû compétitionner mondialement avec des pays aux climats plus chauds, s’adapter aux extrêmes de la météo, au manque de personnel et à la diminution des marges de profit tout en se conformant aux exigences environnementales des plus sévères au monde.</p>
               </div>
             </div>
             <!--/ .row-->
 
             <div class="row">
               <div class="col-12">
-                <p class="p-general-24">Pour travailler dans ces conditions, les producteurs doivent prendre des décisions complexes très importantes afin d’assurer la pérennité de leurs entreprises.</p>
-                <p class="p-general-24">Fort heureusement, la plupart des producteurs ont relevé ces défis en misant sur l’aide des agronomes – conseils et sur leur propre formation. Ayant suivi attentivement les trois journées de formation en Technologies d’Agriculture de Précision du CRAAQ, je les recommande fortement à tous les intervenants. Quoi que assez intenses, vous pourrez continuer votre formation à votre rythme à l’aide d’excellentes notes de cours.</p>
-                <p class="p-general-24">En 2021, l’agriculture numérique est maintenant devenue un choix stratégique et pragmatique, qui tient compte des caractéristiques propres de chaque parcelle, améliore la productivité et répond aux attentes environnementales et sociétales.</p>
-                <p class="p-general-24">Les cartes d’agriculture numérique et leur interprétation sont devenues essentielles aux services – conseils et aux producteurs agricoles. Pour être efficaces, elles doivent être précises et livrées rapidement aux intervenants après leur captation.</p>
-                <p class="p-general-24">Après des années de recherche et de développement, FERMES QUÉBEC a adapté la technologie de pointe des drones à un avion piloté pour des performances optimales.</p>
+                <p class="paragraph">Pour travailler dans ces conditions, les producteurs doivent prendre des décisions complexes très importantes afin d’assurer la pérennité de leurs entreprises.</p>
+                <p class="paragraph">Fort heureusement, la plupart des producteurs ont relevé ces défis en misant sur l’aide des agronomes – conseils et sur leur propre formation. Ayant suivi attentivement les trois journées de formation en Technologies d’Agriculture de Précision du CRAAQ, je les recommande fortement à tous les intervenants. Quoi que assez intenses, vous pourrez continuer votre formation à votre rythme à l’aide d’excellentes notes de cours.</p>
+                <p class="paragraph">En 2021, l’agriculture numérique est maintenant devenue un choix stratégique et pragmatique, qui tient compte des caractéristiques propres de chaque parcelle, améliore la productivité et répond aux attentes environnementales et sociétales.</p>
+                <p class="paragraph">Les cartes d’agriculture numérique et leur interprétation sont devenues essentielles aux services – conseils et aux producteurs agricoles. Pour être efficaces, elles doivent être précises et livrées rapidement aux intervenants après leur captation.</p>
+                <p class="paragraph">Après des années de recherche et de développement, FERMES QUÉBEC a adapté la technologie de pointe des drones à un avion piloté pour des performances optimales.</p>
               </div>
             </div>
             <!--/ .row-->
 
             <div class="row">
               <div class="col-md-6 col-xs-12">
-                <img class="img-padding-card" src="images/agriculture_numerique/CONTROLEUR.jpg" alt="agriculture numerique controleur">
-                <p style="padding:0 8%;"><i>Le contrôleur de vol déclenche la caméra à tous les 60 mètres mesurés au sol.</i></p>
+                <img class="p-all-4" src="images/agriculture_numerique/CONTROLEUR.jpg" alt="agriculture numerique controleur">
+                <p><i>Le contrôleur de vol déclenche la caméra à tous les 60 mètres mesurés au sol.</i></p>
               </div>
               <div class="col-md-6 col-xs-12">
-                <p class="p-card-24">Cette combinaison gagnante permet de capter simultanément en visible, infra-rouge et multi spectral, couvrant 20 parcelles à l’intérieur d’un rayon de 30 Km dans une mission de vol d’une heure et demi, ou de couvrir 1000 acres linéaires par heure.</p>
+                <p class="paragraph m-0 p-all-8">Cette combinaison gagnante permet de capter simultanément en visible, infra-rouge et multi spectral, couvrant 20 parcelles à l’intérieur d’un rayon de 30 Km dans une mission de vol d’une heure et demi, ou de couvrir 1000 acres linéaires par heure.</p>
               </div>
             </div>
             <!--/ .row-->
 
             <div class="row">
               <div class="col-12">
-                <p class="p-general-24">En 2021, FERMES QUÉBEC offre aux agronomes – conseils et aux producteurs  agricoles un dossier complet d’agriculture numérique à <strong>100,00 $</strong> la parcelle comprenant 7 produits:</p>
-                <ol class="p-general-24">
+                <p class="paragraph">En 2021, FERMES QUÉBEC offre aux agronomes – conseils et aux producteurs  agricoles un dossier complet d’agriculture numérique à <strong>100,00 $</strong> la parcelle comprenant 7 produits:</p>
+                <ol class="paragraph o-u-list p-all-4">
                   <li>Carte Orthomosaïque & Rapport de Qualité</li>
                   <li>Carte santé végétale NDVI & Rapport de Qualité</li>
                   <li>Carte d’Élévation DEM & Rapport de Qualité</li>
@@ -211,31 +211,131 @@
 
             <div class="row">
               <div class="col-12">
-                <p class="p-general-24">Conditions: Les parcelles doivent être en groupes de 20 dans les secteurs de <span style="color:#548dd4;">Laurentides sud</span>, <span style="color:#ffd200;">Montérégie Est</span> ou <span style="color:#ff0000;">Montérégie Ouest<span>.</p>
+                <p class="paragraph">Conditions: Les parcelles doivent être en groupes de 20 dans les secteurs de <span style="color:#548dd4;">Laurentides sud</span>, <span style="color:#ffd200;">Montérégie Est</span> ou <span style="color:#ff0000;">Montérégie Ouest<span>.</p>
               </div>
             </div>
             <!--/ .row-->
 
             <div class="row">
               <div class="col-md-6 col-xs-12">
-                <!-- <p class="p-card-24">Actuellement, les missions de vol prévues comptent :</p> -->
-                <p class="p-card-24">
-                  Actuellement, les missions de vol prévues comptent :
-                  <ul class="p-general-24">
-                    <li>000  <span style="color:#548dd4;">Parcelles en Laurentides Sud</span></li>
-                    <li>000  <span style="color:#ffd200;">Parcelles en Montérégie Est</span></li>
-                    <li>000  <span style="color:#ff0000;">Parcelles en Montérégie Ouest</span></li>
-                  </ul>
-                </p>
+                <p class="paragraph m-0 p-all-8">Actuellement, les missions de vol prévues comptent:</p>
+                <ul class="paragraph o-u-list p-left-8 p-right-4">
+                  <li>000  <span style="color:#548dd4;">Parcelles en Laurentides Sud</span></li>
+                  <li>000  <span style="color:#ffd200;">Parcelles en Montérégie Est</span></li>
+                  <li>000  <span style="color:#ff0000;">Parcelles en Montérégie Ouest</span></li>
+                </ul>
               </div>
               <div class="col-md-6 col-xs-12">
-
+                <img class="img-padding-card" src="images/agriculture_numerique/TERRITOIRE.jpg" alt="Territoires Québec missions de vols">
               </div>
             </div>
+            <!--/ .row-->
 
-
+            <div class="row">
+              <div class="col-12">
+                <p class="paragraph p-all-4">Vous pouvez compléter un groupe jusqu’à 20 parcelles ou créer votre propre groupe de 20 parcelles.</p>
+              </div>
             </div>
-            <!--/ .container-->
+            <!--/ .row-->
+
+            <div class="row flex justify-content-ctr">
+              <a href="#" class="button large">Commandez dès maintenant</a>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <h2 class="h2 p-all-4">Examinons ces 7 produits …</h2>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-7 col-xs-12 p-all-4">
+                <h3 class="h3">1-Carte orthomosaïque</h3>
+                <p class="paragraph">À environ 6 Cm/Pixel cette carte cible rapidement les points clés de la parcelle tel les zones de compaction et humides avant les semences. Téléchargée dans votre cellulaire elle facilite l’exploration de la parcelle au sol.</p>
+                <p class="paragraph">Livrée en projection Québec Lambert elle peut être aussi lue avec le logiciel gratuit QGIS.</p>
+              </div>
+              <div class="col-md-5 col-xs-12 p-all-4">
+                <img src="images/agriculture_numerique/VISIBLE.jpg" alt="Carte orthomosaïque">
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-5 col-xs-12 p-all-4">
+                <img src="images/agriculture_numerique/SANTE.jpg" alt="Carte santé">
+              </div>
+              <div class="col-md-7 col-xs-12 p-all-4">
+                <h3 class="h3">2- Carte santé ( NDVI )</h3>
+                <p class="paragraph">L’Indice de Végétation par Différence Normalisée ou NDVI est la plus utilisée car elle mesure la vigueur de la végétation, la couverture et la densité de la canopée et les dommages dus au gel. Plusieurs facteurs influencent les valeurs du NDVI ; la photosynthèse de la plante, son couvert végétal, sa biomasse, son azote, son stress et l’humidité du sol.</p>
+                <p>Lien pour d’autres indices de végétation: </p>
+                <a href="https://www.youtube.com/watch?v=p7P3A8xswA8">Lien</a>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-7 col-xs-12 p-all-4">
+                <h3 class="h3">3- Carte d’élévation ( DEM ) </h3>
+                <p class="paragraph">Cette carte topographique est utile pour le nivèlement, le drainage et l’irrigation des champs.</p>
+                <p class="paragraph">Cette carte, ainsi que les deux autres cartes précédentes sont livrées avec un rapport de qualité indiquant les métriques obtenues.</p>
+              </div>
+              <div class="col-md-5 col-xs-12 p-all-4">
+                <img src="images/agriculture_numerique/ELEVATION360x467.jpg" alt="Carte d'élévation">
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <h3 class="h3">4- MODÈLE 3D</h3>
+              <p class="paragraph">Très avantageux dans l’arsenal d’outils de l’agronome et du producteur, On peut y mesurer facilement les distances, surfaces et volumes. Les lecteurs 3D gratuits  QGIS ou Drone Deploy sont faciles d’utilisation.</p>
+            </div>
+            <!--/ .row -->
+
+            <div class="row">
+              <img class="p-left-8 p-right-8 p-bottom-8" src="images/agriculture_numerique/Mesure Volume.jpg" alt="Modèle 3D mesure volume">
+            </div>
+            <!--/ .row -->
+
+            <div class="row">
+              <div class="col-md-6 col-xs-12 p-all-4">
+                <h3 class="h3">5- OrthoPhotos 50 Méga Pixels</h3>
+                <p class="paragraph">Les ortho photos 50 Méga Pixels géo- référencées ont une résolution jusqu’à 4 Cm/Pixel, soit 25 fois plus grande que par satellite et permettent un examen visuel très détaillé des champs sans avoir à vous déplacer. Vous pourrez entre autres localiser certains débris le long des parcelles adjacentes aux routes. Elles servent aussi à la création de cartes Orthomosaïques.</p>
+              </div>
+              <div class="col-md-6 col-xs-12 p-all-4 p-all-8">
+                <img src="images/agriculture_numerique/ORTHOPHOTO.jpg" alt="Orthophoto">
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-6 col-xs-12">
+                <img src="images/agriculture_numerique/ORTHO-VIDEO.jpg" alt="Carte santé">
+              </div>
+              <div class="col-md-6 col-xs-12 p-all-4">
+                <h3 class="h3">6- Ortho Vidéo format 4K</h3>
+                <p class="paragraph">Cette vidéo considère l’environnement de la parcelle pour approfondir son étude. On peut même parfois trouver des explications et solutions en comparant les images des alentours.</p>
+              </div>
+            </div>
+            <!--/ .row-->
+
+            <div class="row">
+              <div class="col-md-6 col-xs-12 p-all-4">
+                <h3 class="h3">7- Thermographies 740 Ko</h3>
+                <p class="paragraph">Ces images en fausses couleurs démontrent intuitivement les variations de température sur la parcelle.</p>
+                <p class="paragraph">L’infra – rouge est idéal pour vérifier l’assèchement des parcelles au printemps. Il est aussi très utile pour vérifier la conformité des systèmes d’irrigation, ce qui est essentiel pour maximiser les rendements.</p>
+                <p class="paragraph">Les thermographies radiométriques ou l’on peut mesurer directement les températures sont en option.</p>
+              </div>
+              <div class="col-md-6 col-xs-12 p-all-4 p-all-8">
+                <img src="images/agriculture_numerique/THERMOGRAPHIE.jpg" alt="Termographie">
+              </div>
+            </div>
+            <!--/ .row-->
+
+
+
+
+          </div>
+          <!--/ .container-->
 
         </section>
         <!--/ .section-->
